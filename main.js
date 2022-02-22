60007e6e9c8d7cb008c232b1c7738b51ee49faa8
LAKSHYA SINGH CHAUHAN <85747258+lakshyagithub@users.noreply.github.com>
2022-02-22 19:28:46 +0530

    I had to re-upload these
    The site is not working so reuploaded these

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ function note() {
     window.alert("Note: Play different sounds like clap, spoon and cup, bell to make the aliens dance");
 }
 function startClassification() {
-    navigator.mediaDevices.getUserMedia({audio=true});
+    navigator.mediaDevices.getUserMedia({   audio=true  });
     classifier = ml5.soundClassifier('https://storage.googleapis.com/tm-model/AiQhG2JR0/model.json', modelReady);
 }
